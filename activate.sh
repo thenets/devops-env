40e3d4dede561c4a7160df6571d50696c8edb988
Luiz Costa <luiz@thenets.org>
2019-10-04 01:40:43 -0300

    Hashicorp Vault login working

--- activate.sh
+++ activate.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e
-
 source $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/src/vars.sh
 
 set -o allexport
@@ -12,6 +10,9 @@ if ! [[ -d ${DEVOPS_ENV_DIR} ]]; then
     log_error "        You should run ./devops/install.sh"
     exit 1
 fi
+
+# Get original PS1 before any change
+ORIGINAL_PS1=${PS1}
 
 # Loads Python virtualenv
 source ${DEVOPS_PYTHON_ENV_DIR}/bin/activate
@@ -33,20 +34,15 @@ export PATH=${PATH}:${DEVOPS_ENV_DIR}/bin
 export PATH=${PATH}:${DEVOPS_DIR}/bin
 
 # Style for bash
-export PS1='(DevOps) \[\e]0;\u@\h: \w\a\]\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]$ '
+#export PS1='(DevOps) \[\e]0;\u@\h: \w\a\]\[\033[01;32m\]\u\[\033[00m\]:\[\033[01;34m\]\W\[\033[00m\]$ '
+export PS1="(DevOps) ${ORIGINAL_PS1}"
 
 # Ansible settings
 export ANSIBLE_HOST_KEY_CHECKING=False
 export DEFAULT_VAULT_PASSWORD_FILE=${DEVOPS_SECRETS_DIR}/ansible_vault_secret
 alias ansible-playbook='ansible-playbook -i ${DEVOPS_DIR}/ansible/hosts.ini --vault-password-file ${DEFAULT_VAULT_PASSWORD_FILE}'
 
-# # Do Vault login
-# unset VAULT_TOKEN
-# export VAULT_TOKEN=$(vault login -no-store -method=userpass username=${VAULT_USERNAME} password=${VAULT_PASSWORD} -format=json 2>/dev/null | jq '.auth.client_token' -r)
-# ${DIR}/setup-vault.sh
-
-# # Vault settings
-# alias vault-secrets-download="${DIR}/vault-secrets-download.sh"
-# alias vault-secrets-upload="${DIR}/vault-secrets-upload.sh"
+# Do Hashicorp Vault login
+source ${DEVOPS_SRC_DIR}/hashicorp_vault/get-token.sh
 
 set +o allexport
---
+++ src/hashicorp_vault/get-token.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ ${DEVOPS_DIR} == "" ]]; then
+    echo "[ERROR] This file should not be executed directly!"
+    exit 1
+fi
+
+if [[ ${VAULT_ADDR} != "https://server-url:8080" ]] && [[ ${VAULT_ADDR} != "" ]]; then
+    unset VAULT_TOKEN
+    export VAULT_TOKEN=$(vault login -no-store -method=userpass username=${VAULT_USERNAME} password=${VAULT_PASSWORD} -format=json 2>/dev/null | jq '.auth.client_token' -r)
+fi
