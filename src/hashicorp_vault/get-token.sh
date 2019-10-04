64ecd95a2f4200354e74ff63194957aa4b46df47
Luiz Costa <luiz@thenets.org>
2019-10-04 03:56:26 -0300

    Allow secrets management without env activated

--- src/hashicorp_vault/get-token.sh
+++ src/hashicorp_vault/get-token.sh
@@ -7,5 +7,5 @@ fi
 
 if [[ ${VAULT_ADDR} != "https://server-url:8080" ]] && [[ ${VAULT_ADDR} != "" ]]; then
     unset VAULT_TOKEN
-    export VAULT_TOKEN=$(vault login -no-store -method=userpass username=${VAULT_USERNAME} password=${VAULT_PASSWORD} -format=json 2>/dev/null | jq '.auth.client_token' -r)
+    export VAULT_TOKEN=$(${DEVOPS_ENV_DIR}/bin/vault login -no-store -method=userpass username=${VAULT_USERNAME} password=${VAULT_PASSWORD} -format=json 2>/dev/null | jq '.auth.client_token' -r)
 fi
