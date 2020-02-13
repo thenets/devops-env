d77f09d3aca066889df361555dd131561c05c74a
Luiz Costa <phelip@thenets.org>
2020-02-13 04:31:23 -0300

    Fix installation if .gitignore don't exist

--- src/update-gitignore.sh
+++ src/update-gitignore.sh
@@ -5,14 +5,20 @@ source $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/vars.sh
 log_info "[devops_env] Updating .gitignore file"
 
 # Check if .gitignore has \n in the tail
-if ! [[ -z "$(tail -c 1 ${DEVOPS_PROJECT_DIR}/.gitignore)" ]]; then
-    echo "" >> ${DEVOPS_PROJECT_DIR}/.gitignore
+if [[ -f ${DEVOPS_PROJECT_DIR}/.gitignore ]]; then
+    if ! [[ -z "$(tail -c 1 ${DEVOPS_PROJECT_DIR}/.gitignore)" ]]; then
+        echo "" >> ${DEVOPS_PROJECT_DIR}/.gitignore
+    fi
 fi
 
 # Add new items to .gitignore file
 FILES_TO_IGNORE=$(echo "config.ini .env/ secrets/ .terraform/ *.retry" | sed 's/ /\n/g')
-for FILE in ${FILES_TO_IGNORE}; do
-    if ! grep -Fxq "${FILE}" ${DEVOPS_PROJECT_DIR}/.gitignore; then
-        echo "${FILE}" >> ${DEVOPS_PROJECT_DIR}/.gitignore
-    fi
-done
+if [[ -f ${DEVOPS_PROJECT_DIR}/.gitignore ]]; then
+    for FILE in ${FILES_TO_IGNORE}; do
+        if ! grep -Fxq "${FILE}" ${DEVOPS_PROJECT_DIR}/.gitignore; then
+            echo "${FILE}" >> ${DEVOPS_PROJECT_DIR}/.gitignore
+        fi
+    done
+else
+    echo "${FILES_TO_IGNORE}" >> ${DEVOPS_PROJECT_DIR}/.gitignore
+fi
