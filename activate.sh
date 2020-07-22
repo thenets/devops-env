a26ac2bf191c477342d7d6ffe549f91a2071e4ed
Luiz Costa <phelip@thenets.org>
2020-07-22 01:55:18 -0300

    Force some env clean up

--- activate.sh
+++ activate.sh
@@ -10,6 +10,7 @@ fi
 if [[ ${PS1} == "["*"|"*"]"* ]]; then
     log_warning "[devops_env] Already activated! Reloading..."
     deactivate
+    unset $(awk 'BEGIN{for(v in ENVIRON) print v}' | grep ^AWS)
 fi
 
 source $( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/src/vars.sh
