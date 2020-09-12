a5e64ad3c160544b1d4170c71f200ae705410f55
Luiz Costa <phelip@thenets.org>
2020-09-12 17:06:39 -0300

    Disable kubectl autocomplete

--- plugins/activate/kubectl-autocomplete.sh
+++ plugins/activate/kubectl-autocomplete.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if [[ -f "/usr/share/bash-completion/bash_completion" ]]; then
-    source /usr/share/bash-completion/bash_completion
-fi
+# if [[ -f "/usr/share/bash-completion/bash_completion" ]]; then
+#     source /usr/share/bash-completion/bash_completion
+# fi
 
-if [[ -f "${DEVOPS_ENV_DIR}/bin/kubectl" ]]; then
-    source <(${DEVOPS_ENV_DIR}/bin/kubectl completion bash)
-fi
+# if [[ -f "${DEVOPS_ENV_DIR}/bin/kubectl" ]]; then
+#     source <(${DEVOPS_ENV_DIR}/bin/kubectl completion bash)
+# fi
